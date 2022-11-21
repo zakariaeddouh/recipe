88dbf443afdb5986597aeaaf299fc84f44fdae83
zakariaeddouh <zakaria.eddouh@gmail.com>
2022-11-21 20:56:47 +0100

    Security: Registration Form

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\RegistrationType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -15,7 +19,7 @@ class SecurityController extends AbstractController
      * @param AuthenticationUtils $authenticationUtils
      * @return Response
      */
-    #[Route('/login', name: 'app_login', methods: ['GET', 'POST'])]
+    #[Route('/login', name: 'security_login', methods: ['GET', 'POST'])]
     public function index(
         AuthenticationUtils $authenticationUtils
     ): Response {
@@ -33,10 +37,47 @@ class SecurityController extends AbstractController
      *
      * @return void
      */
-    #[Route('/logout', name: 'app_logout', methods: ['GET'])]
+    #[Route('/logout', name: 'security_logout')]
     public function logout()
     {
         // controller can be blank: it will never be called!
         throw new \Exception('Don\'t forget to activate logout in security.yaml');
+    }
+
+    /**
+     * This controller allow us to register 
+     *
+     * @param Request $request
+     * @param EntityManagerInterface $manager
+     * @return request
+     */
+    #[Route('/registration', name: 'security_registration', methods: ['GET', 'POST'])]
+    public function registration(
+        Request $request, 
+        EntityManagerInterface $manager
+    ): Response {
+        $user = new User();
+        $user->setRoles(['ROLE_USER']);
+
+        $form = $this->createForm(RegistrationType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = $form->getData();
+
+            $this->addFlash(
+                'success',
+                'Votre compte a bien été créé.'
+            );
+
+            $manager->persist($user);
+            $manager->flush();
+
+            return $this->redirectToRoute('security_login');
+        }
+
+        return $this->render('pages/security/registration.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -41,7 +41,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     #[ORM\Column]
     #[Assert\NotBlank()]
-    private ?string $password = null;
+    private ?string $password = 'password';
 
     #[ORM\Column]
     #[Assert\NotBlank()]
