1b981e3926c5c62939a402a9aea39bdd989802ba
zakariaeddouh <zakaria.eddouh@gmail.com>
2022-11-21 12:16:52 +0100

    Security: Firewall & Login Form

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+    /**
+     * This controller allow us to login
+     *
+     * @param AuthenticationUtils $authenticationUtils
+     * @return Response
+     */
+    #[Route('/login', name: 'app_login', methods: ['GET', 'POST'])]
+    public function index(
+        AuthenticationUtils $authenticationUtils
+    ): Response {
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('pages/security/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error'         => $error,
+        ]);
+    }
+
+    /**
+     * This controller allow us to logout
+     *
+     * @return void
+     */
+    #[Route('/logout', name: 'app_logout', methods: ['GET'])]
+    public function logout()
+    {
+        // controller can be blank: it will never be called!
+        throw new \Exception('Don\'t forget to activate logout in security.yaml');
+    }
+}
