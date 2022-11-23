71e492e3c7b124eaef87b70d37e83ad91abbde3c
zakariaeddouh <zakaria.eddouh@gmail.com>
2022-11-23 13:36:50 +0100

    Share a recipe

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,18 +3,19 @@
 namespace App\Controller;
 
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\RecipeRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     #[Route('/', 'home_index', methods: ['GET'])]
     public function index(
-        
+        RecipeRepository $repository
     ): Response {
         return $this->render('pages/home.html.twig', [
-            
+            'recipes' => $repository->findPublicRecipe(null),
         ]);
     }
 }
